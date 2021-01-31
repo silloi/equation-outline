c88c42c2ea2d0ec74b7b1339a9b03ccf0d43b472
silloi <silloi1993@gmail.com>
2021-01-31 21:24:41 +0900

    feat: add input fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
+import { BlockMath } from 'react-katex';
 
 interface Formula {
   ref1: number
@@ -12,17 +17,7 @@ interface Formula {
 
 type Props = {
   formulae: Formula[];
-};
-
-// const First = (props: Props) => {
-//   const { formulae } = props
-
-//   const refEq = (ref: number[]) => '(' + ref.join('. ') + ')'
-
-//   return (
-//     <
-//   )
-// }
+}
 
 const Line = (props: Props) => {
   const { formulae } = props
@@ -38,19 +33,57 @@ const Line = (props: Props) => {
 
   return (
     <ul>
-      {formulae.map(formula => <li key={formula.eq}>
-        <BlockMath math={`${refEq(formula)} ${formula.eq}`} />
-      </li>)}
+      {formulae.map(formula => <List key={formula.eq}>
+        <ListItem>
+          <BlockMath math={`${refEq(formula)} ${formula.eq}`} />
+        </ListItem>
+      </List>)}
     </ul>
   );
 }
 
 export const Home = () => {
-  const [formulae, setFormulae] = useState([{ ref1: 1, ref2: 1, eq: '\\int_0^\\infty x^2 dx'}, { ref1: 1, ref2: 2, eq: '\\int_0^\\infty x^2 dx'}])
+  const [formulae, setFormulae] = useState([])
 
-  // const [indent1, setIndent1] = useState([])
+  useEffect(() => {
+    setFormulae([])
+  }, [])
 
-  // const [indent2, setIdnent2] = useState([])
+  const [input, setInput] = useState({ ref1: null, ref2: null, eq: '' })
+
+  const handleInput = (e: { target: { name: any; value: any; }; }) => {
+    switch (e.target.name) {
+      case 'eq':
+        setInput({
+          ref1: input.ref1,
+          ref2: input.ref2,
+          eq: e.target.value
+        });
+        break;
+      case 'ref1':
+        setInput({
+          ref1: e.target.value,
+          ref2: input.ref2,
+          eq: input.eq
+        });
+        break;
+      case 'ref2':
+        setInput({
+          ref1: input.ref1,
+          ref2: e.target.value,
+          eq: input.eq
+        });
+        break;
+      default:
+        console.log('key not found');
+    }
+  }
+
+  const addEquation = (eq: { ref1: number, ref2: number, eq: string }) => {
+    setFormulae([...formulae, eq])
+
+    setInput({...input, ref2: input.ref2 + 1})
+  }
 
   return (
     <div className="container">
@@ -59,167 +92,15 @@ export const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
   
-      <Line formulae={formulae}/>
-  
       <main> 
-        <InlineMath>\int_0^\infty x^2 dx</InlineMath>
-        <BlockMath>\int_0^\infty x^2 dx</BlockMath>
+        <div style={{ marginTop: 10 }}>
+          <TextField id="outlined-basic" name="ref1" label="Input ref1" type="number" variant="outlined" value={input.ref1} onChange={handleInput} />
+          <TextField id="outlined-basic" name="ref2" label="Input ref2" type="number"variant="outlined" value={input.ref2} onChange={handleInput} />
+          <TextField id="outlined-basic" name="eq" label="Input equation" variant="outlined" value={input.eq} onChange={handleInput} />
+          <Button variant="contained" onClick={() => addEquation(input)}>Insert</Button>
+        </div>
+        <Line formulae={formulae}/>
       </main>
-  
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-  
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-  
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-  
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-  
-        footer img {
-          margin-left: 0.5rem;
-        }
-  
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-  
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-  
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-  
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-  
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-  
-        .title,
-        .description {
-          text-align: center;
-        }
-  
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-  
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-  
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-  
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-  
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-  
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-  
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-  
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-  
-        .logo {
-          height: 1em;
-        }
-  
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-  
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-  
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
