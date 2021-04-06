a96697f5cc3f03c4bcf29a02ed79b4d737a64791
西山 雄大 <nishiyamayudai@team-lab.com>
2021-04-06 23:37:01 +0900

    chore: lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import React, { useState, useEffect } from 'react'
 
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
+import 'katex/dist/katex.min.css'
+import { BlockMath } from 'react-katex'
 
 import { connectToDatabase } from '../util/mongodb'
 
@@ -18,7 +18,7 @@ interface Item {
 }
 
 type Props = {
-  itemList: Item[];
+  itemList: Item[]
 }
 
 const Line = (props: Props) => {
@@ -35,16 +35,23 @@ const Line = (props: Props) => {
 
   return (
     <ul>
-      {itemList.map(item => <List key={`${item.ref1}-${item.ref2}`}>
-        <ListItem>
-          <BlockMath math={`${refEq(item)} ${item.eq}`} />
-        </ListItem>
-      </List>)}
+      {itemList.map((item) => (
+        <List key={`${item.ref1}-${item.ref2}`}>
+          <ListItem>
+            <BlockMath math={`${refEq(item)} ${item.eq}`} />
+          </ListItem>
+        </List>
+      ))}
     </ul>
-  );
+  )
 }
 
-export const Home = ({ isConnected, usersData }) => {
+interface HomeProps {
+  isConnected: boolean
+  usersData: any[]
+}
+
+export const Home = (props: HomeProps) => {
   const [itemList, setitemList] = useState([])
 
   useEffect(() => {
@@ -55,33 +62,36 @@ export const Home = ({ isConnected, usersData }) => {
 
   const [fileBase64, setFileBase64] = useState<any>(null)
 
-  const handleInput = (e: { target: { name: any; value: any; }; }) => {
+  const handleInput = (e: { target: { name: any; value: any } }) => {
     switch (e.target.name) {
       case 'eq':
         setInput({
           ref1: input.ref1,
           ref2: input.ref2,
-          eq: e.target.value
-        });
-        break;
+          eq: e.target.value,
+        })
+        break
       case 'ref1':
         setInput({
           ref1: e.target.value,
           ref2: input.ref2,
-          eq: input.eq
-        });
-        break;
+          eq: input.eq,
+        })
+        break
       case 'ref2':
         setInput({
           ref1: input.ref1,
           ref2: e.target.value,
-          eq: input.eq
-        });
-        break;
+          eq: input.eq,
+        })
+        break
     }
   }
 
-  const addEquation = (eq: { ref1: number, ref2: number, eq: string }, response?: string) => {
+  const addEquation = (
+    eq: { ref1: number; ref2: number; eq: string },
+    response?: string
+  ) => {
     const eqThis = eq
 
     if (response) {
@@ -90,7 +100,7 @@ export const Home = ({ isConnected, usersData }) => {
 
     setitemList([...itemList, eqThis])
 
-    setInput({...input, ref2: input.ref2 + 1})
+    setInput({ ...input, ref2: input.ref2 + 1 })
   }
 
   const convertToBase64 = (file: File) => {
@@ -98,7 +108,7 @@ export const Home = ({ isConnected, usersData }) => {
     fileReader.onload = (e) => {
       setFileBase64(e.target.result)
     }
-    fileReader.readAsDataURL(file);
+    fileReader.readAsDataURL(file)
   }
 
   const inputFile = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,6 +116,7 @@ export const Home = ({ isConnected, usersData }) => {
       return
     }
 
+    // eslint-disable-next-line no-console
     console.log('ID', process.env.MATHPIX_APP_ID)
 
     convertToBase64(e.target.files[0])
@@ -117,20 +128,20 @@ export const Home = ({ isConnected, usersData }) => {
     const payload = {
       src: fileBase64,
       formats: ['text', 'data', 'html'],
-        'data_options': {
-          'include_asciimath': true,
-          'include_latex': true,
-      }
+      data_options: {
+        include_asciimath: true,
+        include_latex: true,
+      },
     }
 
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'content-type': "application/json",
-        'app_id': process.env.MATHPIX_APP_ID,
-        'app_key': process.env.MATHPIX_APP_KEY,
+        'content-type': 'application/json',
+        app_id: process.env.MATHPIX_APP_ID,
+        app_key: process.env.MATHPIX_APP_KEY,
       },
-      body: JSON.stringify(payload)
+      body: JSON.stringify(payload),
     })
 
     const data = await response.json()
@@ -144,29 +155,60 @@ export const Home = ({ isConnected, usersData }) => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-  
-      <main> 
-        {isConnected ? (
+
+      <main>
+        {props.isConnected ? (
           <h2 className="subtitle">You are connected to MongoDB</h2>
-          ) : (
-            <h2 className="subtitle">
-              You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-              for instructions.
-            </h2>
-          )}
-        <p>{usersData[0].name}</p>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+        <p>{props.usersData[0].name}</p>
         <div style={{ marginTop: 10 }}>
-          <TextField id="outlined-basic" name="rfef1" label="Input ref1" type="number" variant="outlined" value={input.ref1} onChange={handleInput} />
-          <TextField id="outlined-basic" name="ref2" label="Input ref2" type="number"variant="outlined" value={input.ref2} onChange={handleInput} />
-          <TextField id="outlined-basic" name="eq" label="Input equation" variant="outlined" value={input.eq} onChange={handleInput} />
-          <Button variant="contained" onClick={() => addEquation(input)}>Insert</Button>
+          <TextField
+            id="outlined-basic"
+            name="rfef1"
+            label="Input ref1"
+            type="number"
+            variant="outlined"
+            value={input.ref1}
+            onChange={handleInput}
+          />
+          <TextField
+            id="outlined-basic"
+            name="ref2"
+            label="Input ref2"
+            type="number"
+            variant="outlined"
+            value={input.ref2}
+            onChange={handleInput}
+          />
+          <TextField
+            id="outlined-basic"
+            name="eq"
+            label="Input equation"
+            variant="outlined"
+            value={input.eq}
+            onChange={handleInput}
+          />
+          <Button variant="contained" onClick={() => addEquation(input)}>
+            Insert
+          </Button>
           <Button component="label">
             Input File
-            <input type="file" onChange={inputFile} style={{opacity: 0, appearance: 'none', position: 'absolute' }} />
+            <input
+              type="file"
+              onChange={inputFile}
+              style={{ opacity: 0, appearance: 'none', position: 'absolute' }}
+            />
           </Button>
-          <Button variant="contained" onClick={() => fetchMathpix()}>Ajax</Button>
+          <Button variant="contained" onClick={() => fetchMathpix()}>
+            Ajax
+          </Button>
         </div>
-        <Line itemList={itemList}/>
+        <Line itemList={itemList} />
       </main>
     </div>
   )
@@ -177,8 +219,8 @@ export async function getStaticProps() {
 
   const isConnected = await client.isConnected()
 
-  const response = await fetch('http://localhost:3000/api/users');
-  const usersData = await response.json();
+  const response = await fetch('http://localhost:3000/api/users')
+  const usersData = await response.json()
 
   return {
     props: { isConnected, usersData },
